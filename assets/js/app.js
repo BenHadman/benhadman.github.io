1a7bb81ad04c8e72ff2afa1d58aa542ce8eac3d1
BenHadman <benhadman@gmail.com>
2018-05-13 13:51:06 +0100

    Added Portfolio Page

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCLfR5pRk48SYOUkhiJlSN-UVnHHUjjGhw",
+    authDomain: "dough-temperature-logger.firebaseapp.com",
+    databaseURL: "https://dough-temperature-logger.firebaseio.com",
+    projectId: "dough-temperature-logger",
+    storageBucket: "dough-temperature-logger.appspot.com",
+    messagingSenderId: "52921623142"
+};
+firebase.initializeApp(config);
